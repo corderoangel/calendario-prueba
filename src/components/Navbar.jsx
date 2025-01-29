c257ee192466df7171793cd6027b0a7f51033be8
corderoangel <corderoan777@gmail.com>
2025-01-28 23:38:00 -0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
+import { Calendar, Menu } from "lucide-react";
+
 const Navbar = () => {
 	return (
-		<nav className="p-4 bg-gray-800 text-white flex justify-between">
-			<p>Navbar</p>
+		<nav className="p-2 text-black flex justify-between items-center">
+			<div className="flex">
+				<Calendar className="mr-2" /> <span className="font-bold">Calendario</span>
+			</div>
+			<div className="flex items-center">
+				<Menu />
+				<img src="https://randomuser.me/api/portraits/women/1.jpg" alt="Foto de perfil" className="rounded-full w-12 h-12 ml-2" />
+			</div>
 		</nav>
 	);
 };
